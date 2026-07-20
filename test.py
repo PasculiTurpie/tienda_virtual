5ef34998f37abf28a1693372b240e3e79dc78e47
Jorge <rodrigo.s.turpie@gmail.com>
2026-07-20 05:14:42 -0400

    feat:inicio de entorno virtual

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+
+respuesta = requests.get("https://www.programacionfacil.org/", timeout=5)
+
+#respuesta2 = requests.get("https://www.gtd.cl/", timeout=5)
+
+contenido = respuesta.content.decode('utf-8')
+
+print(respuesta)
+#print(respuesta2)
+print(contenido)
