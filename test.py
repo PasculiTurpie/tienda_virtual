9b4a17cb890a94fb7f5758113ea4b33003c04ab0
Rodrigo Sepúlveda <rodrigo.s.turpie@gmail.com>
2026-07-20 20:11:58 -0400

    fix: borrado de algunos archivos especiales

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import requests
 
 respuesta = requests.get("https://www.programacionfacil.org/", timeout=5)
 
-#respuesta2 = requests.get("https://www.gtd.cl/", timeout=5)
+respuesta2 = requests.get("https://www.gtd.cl/", timeout=5)
 
 contenido = respuesta.content.decode('utf-8')
 
 print(respuesta)
-#print(respuesta2)
+print(respuesta2)
 print(contenido)
